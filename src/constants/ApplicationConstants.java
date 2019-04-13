199ef79ca1108cc42349eff9eb82e98725e9a081
Marius Durbala <mariusdurbala11@gmail.com>
2019-04-13 13:36:42 +0300

    Hangman grafic interface

--- src/constants/ApplicationConstants.java
+++ src/constants/ApplicationConstants.java
@@ -3,4 +3,7 @@ package constants;
 public class ApplicationConstants {
     public static final String BTN_LOGIN_TEXT = "Login";
     public static final String BTN_LOGOUT_TEXT = "Logout";
+    public static final String USERNAME = "admin";
+    public static final String PASSWORD = "admin";
+
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,29 +2,43 @@ package sample;
 
 import constants.ApplicationConstants;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import sun.rmi.runtime.Log;
 
 public class Controller {
     public TextField Username;
-    public TextField Password;
+    public PasswordField Password;
     public Button Login;
     public TabPane tabPane;
     public Tab tabPlay;
     public Tab tabLogin;
+    public Label lblUsername;
+    public Label lblPassword;
 
     public void loginAction(ActionEvent event) {
         if (Login.getText().equals(ApplicationConstants.BTN_LOGIN_TEXT)){
-            Login.setText(ApplicationConstants.BTN_LOGOUT_TEXT);
+
+            if (Username.getText().equals(ApplicationConstants.USERNAME)&&
+                    Password.getText().equals(ApplicationConstants.PASSWORD)){
+                Login.setText(ApplicationConstants.BTN_LOGOUT_TEXT);
+                lblPassword.setTextFill(Color.GREEN);
+                lblUsername.setTextFill(Color.GREEN);
+            }
+            else {
+                lblPassword.setTextFill(Color.RED);
+                lblUsername.setTextFill(Color.RED);
+            }
         }
         else {
             Login.setText(ApplicationConstants.BTN_LOGIN_TEXT);
+            tabPane.getSelectionModel().select(tabPlay);
+            tabPane.getTabs().get(1).setDisable(true);
         }
     }
 
     public void activateLoginTab(ActionEvent event) {
         tabPane.getTabs().get(1).setDisable(false);
+        tabPane.getSelectionModel().select(tabLogin);
     }
 }
