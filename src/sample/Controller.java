f32f681028a742038d081e183507148aa0e8f193
Marius Durbala <mariusdurbala11@gmail.com>
2019-04-13 13:59:23 +0300

    Hangman grafic interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,8 @@ package sample;
 import constants.ApplicationConstants;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import sun.rmi.runtime.Log;
 
@@ -15,6 +17,7 @@ public class Controller {
     public Tab tabLogin;
     public Label lblUsername;
     public Label lblPassword;
+    public Tab tabCategories;
 
     public void loginAction(ActionEvent event) {
         if (Login.getText().equals(ApplicationConstants.BTN_LOGIN_TEXT)){
@@ -22,8 +25,10 @@ public class Controller {
             if (Username.getText().equals(ApplicationConstants.USERNAME)&&
                     Password.getText().equals(ApplicationConstants.PASSWORD)){
                 Login.setText(ApplicationConstants.BTN_LOGOUT_TEXT);
-                lblPassword.setTextFill(Color.GREEN);
-                lblUsername.setTextFill(Color.GREEN);
+                lblPassword.setTextFill(Color.BLACK);
+                lblUsername.setTextFill(Color.BLACK);
+                tabPane.getTabs().get(2).setDisable(false);
+                tabPane.getSelectionModel().select(tabCategories);
             }
             else {
                 lblPassword.setTextFill(Color.RED);
@@ -34,11 +39,18 @@ public class Controller {
             Login.setText(ApplicationConstants.BTN_LOGIN_TEXT);
             tabPane.getSelectionModel().select(tabPlay);
             tabPane.getTabs().get(1).setDisable(true);
+            tabPane.getTabs().get(2).setDisable(true);
         }
     }
 
     public void activateLoginTab(ActionEvent event) {
         tabPane.getTabs().get(1).setDisable(false);
         tabPane.getSelectionModel().select(tabLogin);
+    }
+
+    public void loginEnterKey(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)){
+            loginAction(null);
+        }
     }
 }
