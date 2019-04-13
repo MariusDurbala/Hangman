190ed7ac60a904263dcd9e35d816e0ad676b4c98
Marius Durbala <mariusdurbala11@gmail.com>
2019-04-13 12:30:25 +0300

    Hangman grafic interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,17 @@ package sample;
 import constants.ApplicationConstants;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 
 public class Controller {
     public TextField Username;
     public TextField Password;
     public Button Login;
+    public TabPane tabPane;
+    public Tab tabPlay;
+    public Tab tabLogin;
 
     public void loginAction(ActionEvent event) {
         if (Login.getText().equals(ApplicationConstants.BTN_LOGIN_TEXT)){
@@ -17,5 +22,9 @@ public class Controller {
         else {
             Login.setText(ApplicationConstants.BTN_LOGIN_TEXT);
         }
+    }
+
+    public void activateLoginTab(ActionEvent event) {
+        tabPane.getTabs().get(1).setDisable(false);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hangman");
+
         primaryStage.setMinWidth(800);
         primaryStage.setMinHeight(600);
+
         Scene scene = new Scene(root,800,600);
 
         primaryStage.setScene(scene);
